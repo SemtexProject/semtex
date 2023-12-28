31b877270d3209b27c3079aff3ff3823322960f6
fuzzy duck <duck@fuzzys-MacBook-Pro.local>
2023-12-28 03:07:04 -0500

    added CLI trading functionality

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits>
 #include "ExchangeClient.h"
 #include <grpcpp/grpcpp.h>
 #include "protos/order.pb.h"
@@ -10,16 +11,43 @@ int main() {
     ExchangeClient client(grpc::CreateChannel(
         "exchange-server:50051", grpc::InsecureChannelCredentials()));
 
-    Order order;
-    order.set_id(1); // Set appropriate values
-    order.set_quantity(10);
-    order.set_price(100.0);
-    order.set_symbol("AAPL");
-    order.set_side(2);
+    std::cout << "Welcome to the trading desk.\n";
 
-    // ... set other order fields
+    while (true) {
+        std::cout << "Enter orders as: [ID] [SIDE] [SYMBOL] [QUANTITY] [PRICE] ";
+        std::cout << "(Enter 'exit' to quit):\n";
 
-    client.SubmitOrder(order);
+        std::string input;
+        std::getline(std::cin, input);
+        if (input == "exit") {
+            break; // Exit the loop if the user types 'exit'
+        }
+
+        std::istringstream iss(input);
+        float quantity, price;
+        std::string symbol, side;
+        int id;
+        
+        if (!(iss >> id >> side >> symbol >> quantity >> price)) {
+            std::cerr << "Invalid input format.\n";
+            continue; // Skip the rest of the loop and start over
+        }
+
+        Order order;
+        order.set_id(id);
+        order.set_quantity(quantity);
+        order.set_price(price);
+        order.set_symbol(symbol);
+        if (side == "BUY") order.set_side(0);
+        else if (side == "SELL") order.set_side(1);
+        else {
+            std::cerr << "Invalid side. Use 'BUY' or 'SELL'.\n";
+            continue; // Skip the rest of the loop and start over
+        }
+
+        // Submit the order
+        client.SubmitOrder(order);
+    }
 
     return 0;
 }
